9f404b67a2059562d369ffb3665012d819975631
Mustafa Soylu <mohkargan@gmail.com>
2024-10-17 17:26:15 +0200

    chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import Graph from "./Graph.tsx";
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
-    <>
+    <ChakraProvider>
       <h1>RDF Graph Visualization</h1>
       <Graph />
-    </>
+    </ChakraProvider>
   );
 }
 
