5022778708ef21555f89ad3bb09481ed013b371f
Mustafa Soylu <mohkargan@gmail.com>
2024-10-21 14:36:54 +0200

    sytling

--- src/graph.d.ts
+++ src/graph.d.ts
@@ -1,12 +1,12 @@
 // Type definitions for the graph data
 type NodeType = {
-    id: string;
+  id: string;
   label?: string;
   group?: string;
-}
+};
 
 type LinkType = {
   source: string;
   target: string;
   label?: string;
-}
+};
